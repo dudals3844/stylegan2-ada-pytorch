c1a040f16eabbfed5c132bc080ac1cc63a1b192f
choiyoungmin <dudals3844@gmail.com>
2021-10-18 11:40:12 +0900

    Modify runway_model.py using exist pkl

--- runway_model.py
+++ runway_model.py
@@ -1,13 +1,22 @@
+import copy
+
 import runway
 import pickle
 import numpy as np
+import torch
 from runway import image
+import legacy
+import dnnlib
+
+
+
 
 @runway.setup(options={'checkpoint': runway.file(extension='.pkl')})
 def setup(opts):
     global Gs
+    opts['checkpoint'] = './pretrain/network-snapshot-000096.pkl'
     with open(opts['checkpoint'], 'rb') as file:
-        G, D, Gs = pickle.load(file)
+        Gs = pickle.load(file)
     return Gs
 
 generate_inputs = {
@@ -20,10 +29,18 @@ def convert(model, inputs):
     z = inputs['z']
     truncation = inputs['truncation']
     latents = z.reshape((1, 512))
-    images = model.run(latents, None, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
-    output = np.clip(images[0], 0, 255).astype(np.uint8)
+    device = torch.device('cuda')
+
+    G = model['G_ema'].to(device)
+    label = torch.zeros([1, G.c_dim], device=device)
+    seed = np.random.randint(1000)
+
+    z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+    img = G(z, label, truncation_psi=truncation, noise_mode='random')
+    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    output = img[0].cpu().numpy()
     return {'image': output}
 
 
 if __name__ == '__main__':
-    runway.run()
+    runway.run(host='0.0.0.0', port=3000)
